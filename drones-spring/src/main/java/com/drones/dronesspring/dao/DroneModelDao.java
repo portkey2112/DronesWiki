bd79e0f810830ba910c56509a6bc249b5c5ad2bd
Spoorthi M <spoorthi.m@unbxd.com>
2022-03-09 16:50:06 +0530

    feat: Initial source for spring project on one table.

---
+++ drones-spring/src/main/java/com/drones/dronesspring/dao/DroneModelDao.java
@@ -0,0 +1,10 @@
+package com.drones.dronesspring.dao;
+
+import com.drones.dronesspring.model.DroneModel;
+
+import java.util.List;
+
+public interface DroneModelDao {
+    List<DroneModel>  getAllDroneModels();
+    void insertDroneModel (DroneModel d_model);
+}
---
+++ drones-spring/src/main/java/com/drones/dronesspring/service/DroneModelService.java
@@ -0,0 +1,9 @@
+package com.drones.dronesspring.service;
+
+import com.drones.dronesspring.model.DroneModel;
+import java.util.List;
+
+public interface DroneModelService {
+    List<DroneModel>  getAllDroneModels();
+    void insertDroneModel(DroneModel droneModel);
+}
