c55a659068b723c6e6efff9c31a298ce0658ce72
Spoorthi M <spoorthi2112@gmail.com>
2022-03-11 18:12:56 +0530

    feat(DB setup): Add setup container in docker-compose to create table in the db

---
+++ scripts/setup.sql
@@ -0,0 +1,8 @@
+USE `drones_info`;
+
+
+CREATE TABLE `dronemodel` (`ID` int not null, `name` varchar(255), `users` varchar(255), `country` varchar(255) ,  PRIMARY KEY(`ID`));
+INSERT INTO `dronemodel` VALUES (1,'ghatak', 'military', 'india');
+INSERT INTO `dronemodel` VALUES (3,'Parley SnotBot', 'marine biologists', 'usa');
+INSERT INTO `dronemodel` VALUES (4,'DJI Sheep counter', 'Animal husbandry', 'usa');
+INSERT INTO `dronemodel` VALUES (2, 'AgrasT30', 'Farmers', 'China');
